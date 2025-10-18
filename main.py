3a164dcf1c096bcb843c501904a1dbb3848291e0
FleterEwenn <fleter.ewenn@gmail.com>
2025-10-18 16:19:08 +0200

    Add docx reading and correct some bugs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+from docx import Document
 
 parser = argparse.ArgumentParser(prog="textpipe", description="Moteur de traitement de texte programmable ")
 
@@ -26,13 +27,13 @@ def Split_text(text:str)->str:
 	"""
 	text_toreturn = []
 	chars = [',', '.', ';']
-	for str in text.split(' '):
-		if str[-1] in chars:
-			text_toreturn.append(str[:-1])
-			text_toreturn.append(str[-1])
+	for str_ in text.split(' '):
+		if str_[-1] in chars:
+			text_toreturn.append(str_[:-1])
+			text_toreturn.append(str_[-1])
 
 		else:
-			text_toreturn.append(str)
+			text_toreturn.append(str_)
 	return text_toreturn
 
 def Upper(words:list, wordtoupper:str, all=False)->list:
@@ -52,19 +53,27 @@ def Lower(words:list, wordtolower:str, all=False)->list:
 	for i in range(len(words)):
 		if words[i].lower() == wordtolower:
 			words[i] = words[i].lower()
-	return words	
-
-with open(args.input, 'r') as file:
-	text = file.read()
-	file.close()
-
-current_words = Split_text(text)		
+	return words			
 
 if not args.input or not args.output:
 	raise Exception("il manque un fichier d'entrée ou de sortie")
 
 elif not os.path.isfile(args.input) or not os.path.isfile(args.output):
 	raise Exception("les fichiers mis en paramètre n'existent pas")
+
+if str(args.input).lower().endswith('.docx'):
+	document = Document(args.input)
+	current_words = []
+	for para in document.paragraphs:
+		current_words.append(Split_text(para.text))
+elif str(args.input).lower().endswith('.txt'):
+	with open(args.input, 'r') as file:
+		text = file.read()
+		file.close()
+
+	current_words = Split_text(text)
+else:
+	raise Exception("l'extension du fichier d'entrée n'est pas traitable")
 
 if args.pipeline:
 
@@ -75,17 +84,37 @@ if args.pipeline:
 		if cmd.startswith('replace'):
 
 			current_cmd, old, new = cmd.split(':')
-			current_words = Replace(old, new, current_words)
+
+			if args.input.lower().endswith('.docx'):
+				for i in range(len(current_words)):
+					current_words[i] = Replace(old, new, current_words[i])
+					
+			else:
+				current_words = Replace(old, new, current_words)
 		
 		if cmd.startswith('upper'):
 			current_cmd, word_to_upper = cmd.split(':')
-			current_words = Upper(current_words, word_to_upper)
+
+			if args.input.lower().endswith('.docx'):
+				for i in range(len(current_words)):
+					current_words[i] = Upper(current_words[i], word_to_upper)
+			else:
+				current_words = Upper(current_words, word_to_upper)
 		
 		if cmd.startswith('lower'):
 			current_cmd, word_to_lower = cmd.split(':')
-			current_words = Lower(current_words, word_to_lower)
+			if args.input.lower().endswith('.docx'):
+				for i in range(len(current_words)):
+					current_words[i] = Lower(current_words[i], word_to_lower)
+			else:
+				current_words = Lower(current_words, word_to_lower)
 
+if args.input.lower().endswith('.docx'):
+	for i in range(len(document.paragraphs)):
+		document.paragraphs[i].text = ' '.join(current_words[i])
+	document.save(args.output)
 
-with open(args.output, 'w') as file:
-	file.write(' '.join(current_words))
-	file.close()
+else:
+	with open(args.output, 'w') as file:
+		file.write(' '.join(current_words))
+		file.close()
